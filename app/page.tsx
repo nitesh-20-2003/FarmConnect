1554047d7121c3491bdbe4ec27cb05f796bb6893
Nitesh Sharma <niteshsharmauiet@gmail.com>
2025-07-10 03:01:50 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import LoadingContainer from "@/components/global/LoadingContainer";
 import { Suspense } from "react";
 import {AccordionDemo} from '@/components/home/Accordation'
 import MajorCrops from '@/components/home/MajorCrops'
+import {ChartAreaInteractive} from "@/components/home/Chart";
 function HomPage() {
   return (
     <div>
       <Hero />
-
+        <ChartAreaInteractive />
       <Suspense fallback={<LoadingContainer />}>
         <MajorCrops />
       </Suspense>
